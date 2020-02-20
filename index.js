23984ad7923e114a2e927e60c8908d00d3c7d020
Rukshan Uddin <rukshan.uddin@gmail.com>
2020-02-20 10:20:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+var num1 = 2;
+var num2 = 31;
 
-// Write your code here
+var multiply = num1 * num2;
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+var num1 = 9;
+var num2 = 5;
+
+var mod = num1 % num2;
+
+var num3 = 20;
+
+var max = Math.max(num1, num2, num3)
