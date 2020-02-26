faeec7f3f7a4a3330b7ab373b03b521262c85875
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2020-02-26 14:36:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
 // Write your code here
+let num1 = 31, num2 = 2;
+
+const multiply = num1 * num2;
+
+const random = Math.random() + 1;
+
+const mod = 24 % 5;
+
+const max = Math.max(4, 8, 15, 16, 20);
