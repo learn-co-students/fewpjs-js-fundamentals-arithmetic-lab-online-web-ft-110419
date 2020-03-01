31a864d792bffcf428d0ebcf3a81615141633bd4
Richell Bonfield <richell.bonfield12@gmail.com>
2020-03-01 23:15:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+
+const num1 = 2;
+const num2 = 31;
+
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+let mod = 24 % 10;
+let max = Math.max(1, 10, 15, 20);
