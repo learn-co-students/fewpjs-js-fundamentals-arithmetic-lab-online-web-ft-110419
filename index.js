54aec8558f5a10c62da7ade27021e6cdfc56247b
MaryArbs <mary.arbachauskas@gmail.com>
2020-03-01 15:30:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = (num1 * num2);
 
-// Write your code here
+
+
+const random = Math.floor(Math.random() * 10);
+
+const mod = (24 / 6);
+
+const max = Math.max(1, 2, 3, 4, 20);
